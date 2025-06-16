7bbd7401e11e422f63f21c1279bfe8c153153f68
NagaLalithaPravallika-Jonnala <sagarjonnala84@gmail.com>
2025-06-16 16:22:11 +0530

    Add files via upload

---
+++ Operators/Operators/EqualorNot.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class EqualorNot
+{
+ public static void main(String[] args)
+ {
+ Scanner sc=new Scanner(System.in);
+ int a=sc.nextInt();
+ int b=sc.nextInt();
+ if(a==b)
+ {
+  System.out.println("equal to");
+ }
+ else
+ {
+  System.out.println("not equal to");
+ }
+ } 
+}
---
+++ Operators/Operators/SmallorLarge.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class SmallorLarge
+{
+ public static void main(String[] args)
+ {
+ Scanner sc=new Scanner(System.in);
+ int a=sc.nextInt();
+ int b=sc.nextInt();
+ if(a<b)
+ {
+  System.out.println("a is small,b is large ");
+ }
+ else
+ {
+  System.out.println("a is large,b is small");
+ }
+ 
+ }
+}
