d47ff6213537cfda5a845248b8143faf8392c7d5
NagaLalithaPravallika-Jonnala <sagarjonnala84@gmail.com>
2025-06-17 15:23:57 +0530

    Add files via upload

---
+++ Patterns/Pattern.java
@@ -0,0 +1,69 @@
+/* public class Pattern
+{
+ public static void main(String[] args)
+ {
+  for(int i=0;i<6;i++)
+  {
+   for(int j=0;j<6;j++)
+   {
+   System.out.print("*");
+   }
+   System.out.println();
+  }
+ }
+}
+*/
+
+
+/*public class Pattern
+{
+ public static void main(String[] args)
+ {
+  for(int i=1;i<6;i++)
+  {
+   for(int j=1;j<6;j++)
+   {
+   System.out.print(j);
+   }
+   System.out.println();
+  }
+ }
+}*/
+
+
+
+/*public class Pattern
+{
+ public static void main(String[] args)
+ {
+  for(int i=1;i<6;i++)
+  {
+   for(int j=1;j<6;j++)
+   {
+   System.out.print(i);
+   }
+   System.out.println();
+  }
+ }
+}*/
+
+
+
+public class Pattern
+{
+ static int n;
+ public static void main(String[] args)
+ {
+  for(int i=1;i<n;i++)
+  {
+   for(int j=1;j<n;j++)
+   {
+   System.out.print(i);
+   }
+   System.out.println();
+  }
+ }
+}
+
+
+
